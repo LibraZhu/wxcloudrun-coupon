8198694ef864d28281a7f7af1978adac6731ec66
letin.zhu@gmail.com <letin.zhu@gmail.com>
2022-10-27 16:27:15 +0800

    feat: user/message

---
+++ src/main/java/com/tencent/wxcloudrun/controller/UserMessageController.java
@@ -0,0 +1,36 @@
+package com.tencent.wxcloudrun.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * counter控制器
+ */
+@RestController
+
+public class UserMessageController {
+
+    final Logger logger;
+
+    public UserMessageController() {
+        this.logger = LoggerFactory.getLogger(UserMessageController.class);
+    }
+
+
+    /**
+     * 消息推送
+     *
+     * @return API response json
+     */
+    @GetMapping(value = "/user/message")
+    Object userMessage(Map<String, Object> params) {
+        logger.info("/user/message get request" + new JSONObject(params));
+
+        return "";
+    }
+}
